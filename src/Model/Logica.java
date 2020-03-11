3d45cdbdd453f0daf7629de6469ea2f3b6a4e569
Miguel Angel Rivera <angel1301999@gmail.com>
2020-03-10 22:49:39 -0500

    implementacion de array para la creacion de usuarios

--- src/Model/Logica.java
+++ src/Model/Logica.java
@@ -1,5 +1,27 @@
 package Model;
 
+import java.util.ArrayList;
+import Model.Usuario;
+
+
 public class Logica {
+
+	private ArrayList<Usuario> listaUsuarios;
+
+	public Logica() {
+
+		listaUsuarios = new ArrayList<Usuario>();
+	}
+
+	public void registrarUsuario(String nombreDeUsuario, String correo, String contrasena) {
+		listaUsuarios.add(new Usuario(nombreDeUsuario, contrasena, correo));
+
+		for (Usuario usuario : listaUsuarios) {
+			System.out.println(usuario.getNombreDeUsuario());
+			System.out.println(usuario.getContrasena());
+			System.out.println(usuario.getCorreo());
+			System.out.println("====================");
+		}
+	}
 
 }
